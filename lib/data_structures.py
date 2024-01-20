9c7697f40519e36e2346034a67bdcb88b2ff38c9
Kennedy Musau <kennedy.musauu@gmail.com>
2024-01-20 13:00:29 +0300

    solve get_names

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,23 +16,36 @@ spicy_foods = [
     },
 ]
 
+
 def get_names(spicy_foods):
-    pass
+    return [food["name"] for food in spicy_foods]
+
+
+print(get_names(spicy_foods))
+
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if]
+
+
+print(get_spiciest_foods(spicy_foods))
+
 
 def print_spicy_foods(spicy_foods):
     pass
 
+
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     pass
+
 
 def print_spiciest_foods(spicy_foods):
     pass
 
+
 def get_average_heat_level(spicy_foods):
     pass
+
 
 def create_spicy_food(spicy_foods, spicy_food):
     pass
